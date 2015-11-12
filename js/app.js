9d48f255d22ea5c3c6a8c3d40b343b5b2b2fadf5
JARED SCARR <JAREDSCARR@GMAIL.COM>
2015-11-12 14:51:02 -0800

    added state toggles that work and some styling which sucks

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@ var allPhotos = [];
 var randomGenerate;
 var displayphoto1 = document.getElementById('photoholder1');
 var displayphoto2 = document.getElementById('photoholder2');
+var button = document.getElementById('voteAgainButton');
 var photo1;
 var photo2;
 var update;
 var firstVote = true;
+var voting = true;
 //photo Object constructor
 
 function Photo(theName, path) {
@@ -162,6 +164,11 @@ display = function () {
   newImage2.src = allPhotos[photo2].path;
   newImage2.id = 'right';
   displayphoto2.appendChild(newImage2);
+
+  document.getElementById('captionLeft').innerHTML = allPhotos[photo1].theName;
+  document.getElementById('captionRight').innerHTML = allPhotos[photo2].theName;
+
+  stateVoting();
 }
 
 refreshPhotos = function () {
@@ -176,36 +183,63 @@ refreshPhotos = function () {
   var getRight = document.getElementById('right');
   getLeft.src = allPhotos[photo1].path;
   getRight.src = allPhotos[photo2].path;
+  var changeLeftCaption = document.getElementById('captionLeft');
+  changeLeftCaption.innerHTML = allPhotos[photo1].theName;
+  var changeRightCaption = document.getElementById('captionRight');
+  changeRightCaption.innerHTML = allPhotos[photo2].theName;
 }
 
-displayphoto1.addEventListener('click', function () {
+
+stateVoting = function() {
+  refreshPhotos();
+  var hideChart = document.getElementById('piechart');
+  hideChart.setAttribute('class', 'hide');
+  var hideButton = document.getElementById('voteAgainButton');
+  hideButton.setAttribute('class', 'hide');
+
+  displayphoto1.addEventListener('click', leftEventListener);
+  displayphoto2.addEventListener('click', rightEventListener);
+}
+
+stateResults = function() {
+  //highlight selection
+  displayphoto1.removeEventListener('click', leftEventListener);
+  displayphoto2.removeEventListener('click', rightEventListener);
+  var showChart = document.getElementById('piechart');
+  showChart.removeAttribute('class', 'hide');
+  var showButton = document.getElementById('voteAgainButton');
+  showButton.removeAttribute('class', 'hide');
+  console.log(showButton);
+  //add event listener that calls stateVoting
+  showButton.addEventListener('click', stateVoting);
+}
+
+leftEventListener = function () {
   if (firstVote === true) {
     pieData[photo1].value = 1;
     var ctx = document.getElementById("piechart").getContext("2d");
     var myNewChart = new Chart(ctx).Pie(pieData);
     firstVote === false;
-  }
+    }
   allPhotos[photo1].votes+=1;
   myNewChart.segments[photo1].value = allPhotos[photo1].votes;
   myNewChart.update();
-  refreshPhotos();
-});
-displayphoto2.addEventListener('click', function () {
+  stateResults();
+}
+
+rightEventListener = function () {
   if (firstVote === true) {
     pieData[photo1].value = 1;
     var ctx = document.getElementById("piechart").getContext("2d");
     var myNewChart = new Chart(ctx).Pie(pieData);
     firstVote === false;
-  }
+    }
   allPhotos[photo2].votes+=1;
   myNewChart.segments[photo2].value = allPhotos[photo2].votes;
   myNewChart.update();
-  refreshPhotos();
-});
+  stateResults();
+}
 
-//takes selected photo and highlights it
 
 display();
-
-
 
